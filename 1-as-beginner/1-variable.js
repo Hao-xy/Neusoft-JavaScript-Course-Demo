27bd2925aaef86498d4a872532c3329f05edbb6a
Nathan <2078976818@qq.com>
2021-03-16 16:24:32 +0800

    Update part Course Contents

--- 1-as-beginner/1-variable.js
+++ 1-as-beginner/1-variable.js
@@ -7,23 +7,27 @@
     }
     console.log(x);
 })();
-// var变量不提升
-(function test() {
-    let x = 1;
-    {
-        let x = 2;
-        console.log(x);
-    }
-    console.log(x);
-})();
 
-// 改变值 - 报错
-const name = "Jack";
-name = "Tom";
 
-// 改变引用的属性值 - 不报错
-const obj = {
-    name: "Jack"
-};
-obj.name = "Tom";
-console.log(obj)
+// // var变量不提升
+// (function test() {
+//     let x = 1;
+//     {
+//         let x = 2;
+//         console.log(x);
+//     }
+//     console.log(x);
+// })();
+
+
+
+// // 改变值 - 报错
+// const name = "Jack";
+// name = "Tom";
+
+// // 改变引用的属性值 - 不报错
+// const obj = {
+//     name: "Jack"
+// };
+// obj.name = "Tom";
+// console.log(obj);
