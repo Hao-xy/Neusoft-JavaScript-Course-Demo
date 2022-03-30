4feabb3e043f5e96befc23f3ac4fe9a8d4fdfa89
Hao-xy <2221973128@qq.com>
2022-03-30 18:37:26 +0800

    3.30  18.37

--- 1-as-beginner/1-variable.js
+++ 1-as-beginner/1-variable.js
@@ -9,21 +9,22 @@
 })();
 
 
-// var变量不提升
-(function test() {
-    let x = 1;
-    {
-        let x = 2;
-        console.log(x);
-    }
-    console.log(x);
-})();
+// let变量不提升
+// (function test() {
+//     let x = 1;
+//     {
+//         let x = 2;
+//         console.log(x);
+//     }
+//     console.log(x);
+// })();
 
 
 
-// 改变值 - 报错
+// // 改变值 - 报错
 // const name = "Jack";
 // name = "Tom";
+// console.log(name);
 
 // 改变引用的属性值 - 不报错
 // const obj = {
